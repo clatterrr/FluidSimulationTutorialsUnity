e310337cf6099d99f09355d10b7c9dd36fd83601
clatterrr <1614345603@qq.com>
2021-05-03 13:51:22 +0800

    Newton method

---
+++ Assets/16NewtonMethod/newtonSphere.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newtonSphere : MonoBehaviour
+{
+    public GameObject spherefab;
+    private GameObject ball;
+    private int NowFrame = 0;
+    float valx = 15, valy = 15;//最初猜的值
+    void Start()
+    {
+        ball = Instantiate(spherefab, new Vector3(valx, Fun(0, valx, valy) * 0.001f, valy), Quaternion.identity);
+    }
+    float Fun(float n, float x, float y)
+    {
+        if (n == 0) return (x + 2 * y) * (5 * x + 6 * y) + x - 432;
+        else return -2 * (x + 2 * y) * (5 * x + 6 * y) + y + 854;
+    }
+    void Update()
+    {
+        NowFrame++;
+        if (NowFrame < 60) return;
+        float a = (Fun(0, valx + 1, valy) - Fun(0, valx - 1, valy)) / 2;
+        float b = (Fun(0, valx, valy + 1) - Fun(0, valx, valy - 1)) / 2;
+        float c = (Fun(1, valx + 1, valy) - Fun(1, valx - 1, valy)) / 2;
+        float d = (Fun(1, valx, valy + 1) - Fun(1, valx, valy - 1)) / 2;
+        float e = Fun(0, valx, valy);
+        float f = Fun(1, valx, valy);
+        float div = 1 / (a * d - b * c);
+        float speed = 0.1f;
+        valx = valx - speed * (d * e - b * f) * div;
+        valy = valy - speed * (-c * e + a * f) * div;
+        ball.transform.position = new Vector3(valx, Fun(0, valx, valy) * 0.001f, valy);
+        Debug.Log("x = " + valx + " y = " + valy);
+
+    }
+}
---
+++ Assets/16NewtonMethod/surface.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class surface : MonoBehaviour
+{
+    public int surfacenum = 0;
+    private const int Dimension = 20;
+    Vector3[] verts = new Vector3[Dimension * Dimension];//上一帧的高度
+    protected MeshFilter meshFilter;
+    protected Mesh mesh;
+    int NowFrame = 0;
+    public Material Mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 10;
+        mesh = new Mesh();
+        MeshRenderer meshrender = GetComponent<MeshRenderer>();
+        meshrender.material = Mat;
+        mesh.name = gameObject.name;
+
+        mesh.vertices = GenerateVerts();
+        mesh.triangles = GenerateTries();
+        mesh.RecalculateBounds();
+
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+    }
+    private Vector3[] GenerateVerts()
+    {
+
+        for (int i = 0; i < Dimension; i++)
+        {
+            for (int j = 0; j < Dimension; j++)
+            {
+                float x = i;
+                float y = j;
+                float h = (x + 2 * y) * (5 * x + 6 * y) + x - 432;
+                if (surfacenum == 1) h = -2 * (x + 2 * y) * (5 * x + 6 * y) + y + 854;
+                verts[j * Dimension + i] = new Vector3(x, h * 0.001f, y);
+            }
+        }
+        return verts;
+    }
+
+    private int[] GenerateTries()
+    {
+        var tries = new int[mesh.vertices.Length * 6];
+        for (int i = 0; i < Dimension - 1; i++)
+        {
+            for (int j = 0; j < Dimension - 1; j++)
+            {
+                int TriIdx = (j * (Dimension - 1) + i) * 6;
+                int VerIdx = j * Dimension + i;
+                tries[TriIdx] = VerIdx;
+                tries[TriIdx + 1] = VerIdx + Dimension;
+                tries[TriIdx + 2] = VerIdx + Dimension + 1;
+
+                /*
+                 1 ------  2/4
+                 |             |
+                 |             |
+               0/3 ------- 5
+                 
+                 */
+
+                tries[TriIdx + 3] = VerIdx;
+                tries[TriIdx + 4] = VerIdx + Dimension + 1;
+                tries[TriIdx + 5] = VerIdx + 1;
+            }
+        }
+        return tries;
+    }
+}
